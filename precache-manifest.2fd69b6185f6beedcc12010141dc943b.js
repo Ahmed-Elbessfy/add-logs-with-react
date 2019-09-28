f274c4f36e708005ef5661a1bace888341f563d4
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-09-28 10:02:53 +0200

    Updates

---
+++ precache-manifest.2fd69b6185f6beedcc12010141dc943b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3496f6e36e8f14bf949e3a568ade5bb7",
+    "url": "/add-logs-with-react/index.html"
+  },
+  {
+    "revision": "b81cb1f0954d118536c5",
+    "url": "/add-logs-with-react/static/css/main.504f25d4.chunk.css"
+  },
+  {
+    "revision": "6b65eb549643d6268680",
+    "url": "/add-logs-with-react/static/js/2.873a2e50.chunk.js"
+  },
+  {
+    "revision": "b81cb1f0954d118536c5",
+    "url": "/add-logs-with-react/static/js/main.5d1f9711.chunk.js"
+  },
+  {
+    "revision": "26f95e356d4a0dd92e8d",
+    "url": "/add-logs-with-react/static/js/runtime-main.c0dea5f7.js"
+  }
+]);
